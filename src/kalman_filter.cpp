21f46f3b607ae664325cce6a349da59cad15b817
john-reilly <johnreillymct@outlook.com>
2018-11-15 21:01:58 +0000

    feat add KalmanFilter::Update from Q+A

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -24,8 +24,8 @@ void KalmanFilter::Predict() {
   
   // from Q+A video
   x_ = F_ * x_ ; //section 8 lesson 5
-  MatrixXd Ft = F_.transpose(); //section 9 lesson 5
-  P_ = F_ * P_ * Ft + Q_ ;   // from Q+A video
+  MatrixXd Ft_ = F_.transpose(); //section 9 lesson 5 I note in the Q+A he had Ft no underscore I added one for consistency
+  P_ = F_ * P_ * Ft_ + Q_ ;   // from Q+A video
   
   /**
   TODO:
@@ -38,6 +38,28 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  //// from Q+A video // In section 7 of lesson 5
+  VectorXd y_ = z - H_ * x_ ; // I had z_ but changed to z as per paramete (const VectorXd &z)
+  MatrixXd Ht_ = H_.transpose();
+  MatrixXd S_ = H_ * P_ * Ht_ + R_;
+  MatrixXd Si_ = S_.inverse();
+  MatrixXd K_ = P_ * Ht_ * Si_ ;
+  
+  MatrixXd I_ ; // Identity matrix from quix section 7 of lesson 2
+  
+  //New State // from Q+A video // In section 7 of lesson 5
+  I_ = MatrixXd::Identity(2, 2); //from quiz section 7 of lesson 5 2,2 might be wrong check...
+  x_ = x_ + (K_ * y_);
+  P_ = ( I_ - K_ * H_ ) * P_;
+  //below was added in error from video then deleted....I might use it ...
+ // //KF prediction step // from Q+A video // In section 7 of lesson 5
+  //x_ = F_ * x_ + u_ ;
+  //MatrixXd  Ft_ = F_.transpose() ;
+  //P_ = F_ * P_ * Ft_ + Q_ ;
+  
+  
+  
+  
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
